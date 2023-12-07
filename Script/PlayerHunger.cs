1911efaf4381db0e213ea402b6f20630b5ad8d63
Nicholas Puckdee <132693105+ppuckdee@users.noreply.github.com>
2023-12-07 12:21:18 -0800

    Game Over/ Restart Button added - nick

--- Script/PlayerHunger.cs
+++ Script/PlayerHunger.cs
@@ -1,61 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerHunger : MonoBehaviour
 {
     public bool dead;
-
     public float maxHunger = 100f;
     public float currentHunger;
     public float hungerDecreaseRate = 1f;
     public float hungerIncreaseAmount = 20f;
 
     public Slider hungerSlider;
+    public TMP_Text gameOverText;
 
-    public KeyCode eatKey = KeyCode.E; // The key to press for eating
+    public KeyCode eatKey = KeyCode.E;
+    public KeyCode restartKey = KeyCode.R;
 
     void Start()
     {
-
         dead = false;
-
         currentHunger = maxHunger;
         InvokeRepeating("DecreaseHunger", 1f, 1f);
+        UpdateGameOverText(false);
     }
 
     void Update()
     {
-        if (currentHunger <= 0f)
+        if (!dead && currentHunger <= 0f)
         {
             Debug.Log("Game Over - Starved!");
             Time.timeScale = 0;
             dead = true;
+            UpdateGameOverText(true);
+
+            if (Input.GetKeyDown(restartKey))
+            {
+                RestartGame();
+            }
         }
 
         UpdateHungerUI();
 
-        // Check if the player presses the eat key
-        if (Input.GetKeyDown(eatKey))
+        if (!dead && Input.GetKeyDown(eatKey))
         {
             TryEat();
         }
+
+        if (dead && Input.GetKeyDown(restartKey))
+        {
+            RestartGame();
+        }
+    }
+
+    void RestartGame()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+
+        dead = false;
+        currentHunger = maxHunger;
+        Time.timeScale = 1;
+        UpdateGameOverText(false);
     }
 
     void DecreaseHunger()
     {
-        currentHunger -= hungerDecreaseRate;
-        currentHunger = Mathf.Clamp(currentHunger, 0f, maxHunger);
+        if (!dead)
+        {
+            currentHunger -= hungerDecreaseRate;
+            currentHunger = Mathf.Clamp(currentHunger, 0f, maxHunger);
+        }
     }
 
     void TryEat()
     {
-        // Raycast to detect the object to eat
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
         {
-            // Check if the hit object has a specific tag (e.g., "Food")
             if (hit.collider.CompareTag("Food"))
             {
                 EatObject(hit.collider.gameObject);
@@ -65,10 +87,7 @@ public class PlayerHunger : MonoBehaviour
 
     void EatObject(GameObject objectToEat)
     {
-        // Destroy the object (you can replace this with your specific logic)
         Destroy(objectToEat);
-
-        // Increase hunger when eating
         currentHunger += hungerIncreaseAmount;
         currentHunger = Mathf.Clamp(currentHunger, 0f, maxHunger);
     }
@@ -78,6 +97,14 @@ public class PlayerHunger : MonoBehaviour
         if (hungerSlider != null)
         {
             hungerSlider.value = currentHunger / maxHunger;
+        }
+    }
+
+    void UpdateGameOverText(bool show)
+    {
+        if (gameOverText != null)
+        {
+            gameOverText.gameObject.SetActive(show);
         }
     }
 }
